140c69fb6899a1184754aa51026acbf9d7e618b8
Krist Papadopoulos <kristpapas@gmail.com>
2022-06-14 08:04:47 -0400

    updated to tensorflow 2.6.2 and added constraint parameters for alpha

--- ses.py
+++ ses.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-Tensorflow/Keras implementation of Simple Exponential Smoothing.
-
-Layer define to learn parameters: alpha and initial level
-as per component form of simple exponential smoothing
-defined from Forecasting: Principles and Practice
-by Hyndman and George Athanasopoulos https://otexts.com/fpp2/
-
-Reference: https://github.com/mcskinner/ets
-
-Author:    Krist Papadopoulos
-V0 Date:   July 6, 2019
-        
-           tensorflow==1.10.1
-"""
-
 import tensorflow as tf
 
 class SES(tf.keras.layers.Layer):
-    def __init__(self, dtype=tf.float32):
+    """
+    Tensorflow/Keras implementation of Simple Exponential Smoothing.
+
+    Layer define to learn parameters: alpha
+    as per component form of simple exponential smoothing
+    defined from Forecasting: Principles and Practice
+    by Hyndman and George Athanasopoulos https://otexts.com/fpp2/
+
+    Reference: https://github.com/mcskinner/ets
+
+    Author:    Krist Papadopoulos
+    V0 Date:   July 6, 2019
+               tensorflow==1.10.1
+    V1 Date:   June 14, 2022
+               tensorflow==2.6.2
+    """
+    def __init__(self, min_constraint, max_constraint, dtype=tf.float32):
         super(SES, self).__init__()
+        self.min_constraint = min_constraint
+        self.max_constraint = max_constraint
     
     def build(self, input_shape):
-        self.alpha = self.add_weight('alpha', shape=[1,], 
-                                     initializer=tf.keras.initializers.random_uniform(0,1), 
-                                     constraint=tf.keras.constraints.min_max_norm(0,1))
+        self.alpha = self.add_weight('alpha', 
+                                     shape=[1,], 
+                                     initializer=tf.keras.initializers.random_uniform(0,1),
+                                     regularizer=tf.keras.regularizers.L2(0.1),
+                                     constraint=tf.keras.constraints.min_max_norm(self.min_constraint,
+                                                                                  self.max_constraint))
         
-        self.level = self.add_weight('level', shape=[1,], 
-                                     initializer=tf.keras.initializers.truncated_normal())
-        
-    def call(self, input):
+    def call(self, timeseries):
         
         def ses(y, alpha, level):
-            '''Simple exponential smoothing using component form
-            from Forecasting: Principles and Practice - Hyndman and George Athanasopoulos'''
+            '''Apply simple exponential smoothing'''
             forecast = level
             updated_level = forecast + alpha * (y - forecast)
             return forecast, updated_level
         
         predictions = []
-        for time_step in range(input.shape[0]):
-            prediction, self.level = ses(input[time_step], self.alpha, self.level)
+        level = tf.reshape(timeseries[0], 1)
+        for time_step in timeseries[1:]:
+            prediction, level = ses(time_step, self.alpha, level)
             predictions.append(prediction)
-         
-        return tf.concat(predictions, axis=-1)
+        return tf.concat(predictions, -1)
